79694cb1094ab0eda973bda9a869b789bc68b6c0
Sade Nesmith <sadework21@gmail.com>
2024-07-10 17:00:57 -0400

    Initial Commit

---
+++ MVVM-model/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM-model
+//
+//  Created by sade on 7/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '17.5.0'
+
+target 'MVVM-model' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for MVVM-model
+  pod 'Alamofire'
+
+end
